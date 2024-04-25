d4fd7c555d35fbb726fbf4ad3b1d6429dce46d67
Md. Mehedi Hasan Naim <90564815+naim79992@users.noreply.github.com>
2024-04-25 23:13:06 +0800

    -

---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,26 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/hellodr.png",
+    "assets/projects/KMCash.png",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/flutter.png",
+    "assets/projects/flutter.png",
+  ];
+
+  static const List<String> titles = [
+    "Hello Dr.",
+    "KMCash",
+  ];
+
+  static const List<String> description = [
+    "It,s my portfolio website",
+    "It,s a Java based Studennt management system.",
+  ];
+
+  static const List<String> links = [
+    "https://github.com/naim79992/Portfolio",
+    "https://github.com/naim79992/Student-management-system",
+  ];
+}
